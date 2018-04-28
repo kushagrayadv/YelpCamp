58c7e408b646735b774ac41071a2efbd28b53b24
Kushagra Yadav <kushagrayadv15@gmail.com>
2018-04-28 21:36:21 +0000

    add mongolab

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ var commentRoutes       = require("./routes/comments"),
 
 var app = express();
 
-mongoose.connect("mongodb://localhost/Yelp_Camp");
-// mongoose.connect("mongodb://kushyadv:baapbolpehle@ds161529.mlab.com:61529/yelp_camp");
+// mongoose.connect("mongodb://localhost/Yelp_Camp");
+mongoose.connect("mongodb://kushyadv:baapbolpehle@ds161529.mlab.com:61529/yelp_camp");
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
